dd84d9bc9684eeb5455187ff3568d130f3a29309
unknown <camerondedore@gmail.com>
2019-04-11 22:49:13 -0700

    misc

--- Assets/C#/FX/FXFactionColor.cs
+++ Assets/C#/FX/FXFactionColor.cs
@@ -16,7 +16,9 @@ public class FXFactionColor : MonoBehaviour
 {
     #region Fields
     public static Color[] factionColors = { new Color(.5f, .5f, .5f), new Color(.2f, .4f, .8f), new Color(.8f, .2f, .2f), new Color(.8f, .6f, .3f), new Color(.2f, .8f, .5f) };
-    #endregion
+
+	[SerializeField] float bloom = 1;
+	#endregion
 
     #region Properties
     #endregion
@@ -36,8 +38,10 @@ public class FXFactionColor : MonoBehaviour
         yield return null;
 
         var faction = transform.root.GetComponent<IFaction>().GetFaction();
-        GetComponent<Renderer>().material.SetColor("_Color", factionColors[faction]);
-        GetComponent<Renderer>().material.SetColor("_EmissionColor", factionColors[faction]);
+		var newColor = factionColors[faction];
+		newColor.a = bloom;
+		GetComponent<Renderer>().material.SetColor("_Color", newColor);
+        GetComponent<Renderer>().material.SetColor("_EmissionColor", newColor);
     }
     #endregion
 }
